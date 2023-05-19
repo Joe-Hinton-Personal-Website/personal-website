7d25cb5b94d17f27b15a3d8d29dfa54acd8751a4
JoeHinton <hintonmjoseph@gmail.com>
2023-05-18 19:16:34 -0500

    add a lot

---
+++ fullstack/block20/Unit2.FreelancerForum.Starter/index.js
@@ -0,0 +1,28 @@
+// NOT WORKING OR FINISHED!!!
+// I DO NOT UNDERSTAND THIS!!!
+const users = [
+    { name: "John", age: 25, occupation: "gardener" },
+    { name: "Lenny", age: 51, occupation: "programmer" },
+    { name: "Andrew", age: 43, occupation: "teacher" },
+    { name: "Peter", age: 81, occupation: "teacher" },
+    { name: "Anna", age: 43, occupation: "teacher" },
+    { name: "Albert", age: 76, occupation: "programmer" },
+    { name: "Adam", age: 47, occupation: "teacher" },
+    { name: "Robert", age: 72, occupation: "driver" },
+];
+
+// function main() {
+//   const rootElement = document.getElementById("root");
+//   const h1Element = document.createElement("h1");
+//   h1Element.innerHTML = "FREELANCERS";
+//   rootElement.appendChild(h1Element);
+//   for (let i = 0; i < users.length; i++) {
+//     const listElement = document.createElement("li"); 
+//     listElement.innerHTML = users[i];
+//     orderedList.appendChild(listElement);
+//   }
+//   rootElement.appendChild(orderedList);
+// }
+
+// //call the main function
+// main();
